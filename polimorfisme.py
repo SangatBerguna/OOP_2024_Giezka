bdd36e54641c96a1d47b989fdb4c1870d79dab2d
Giezka_Ganteng <151720301+SangatBerguna@users.noreply.github.com>
2024-12-09 09:47:22 +0700

    polymorphisme
    polymorphisme

---
+++ polimorfisme.py
@@ -0,0 +1,29 @@
+# prompt: buatin kode polimorfisme
+
+class Gojek:
+    def __init__(self, name):
+        self.name = name
+
+    def speak(self):
+        raise NotImplementedError("Subclass must implement abstract method")
+
+
+class GoRide(Gojek):
+    def speak(self):
+        return "Perjalanan pendek"
+
+
+class GoCar(Gojek):
+    def speak(self):
+        return "Taksi online"
+
+
+class GoFood(Gojek):
+    def speak(self):
+        return "Pesan-antar makanan"
+
+
+Gojek = [GoRide("Buddy"), GoCar("Whiskers"), GoFood("Daisy")]
+
+for Gojek in Ojek Online:
+    print(Gojek.name + " says " + Gojek.speak())
